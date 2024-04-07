3dab9d50632d5a0b97527678999f10b17f67598f
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-07 00:44:00 +0000

    5. 00...99

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+j = 0
+for i in range(0, 10):
+    for j in range (j + i)
+        if i < 99:
+            print("{:02d}".format(j),  end=', ')
+        else:
+            print("{}".format(i))
