de2f7dd58f65d94f98ab3188893fa74eeaeceb64
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-07 03:27:03 +0000

    6. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 89):
-    flag = False 
-    for j in range(10,90,10):
-        if i == j:        
-            flag = True
-            break
+for i in range(0, 10):
+    for x in range(i + 1, 10):
+        print("{}".format(i), end='')
+        if i == 8 and x == 9:
+            print("{:d}".format(x))
         else:
-            i + 1
-            print("{:02d}".format(i),  end=', ')
-
+            print("{:d}".format(x), end=', ')
