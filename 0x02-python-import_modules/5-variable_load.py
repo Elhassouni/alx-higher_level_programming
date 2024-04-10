0081e9be4e46d76a3d89821f2c0e99f70834275e
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-10 04:52:31 +0000

    5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import variable_load_5
+if __name__ == "__main__":
 
-result = variable_load_5.a
-print("{}".format(result))
+    result = variable_load_5.a
+    print("{}".format(result))
