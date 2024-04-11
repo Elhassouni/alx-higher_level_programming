94b836d105d74b9eee8f7283d0e2caeee9d4a3f2
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-11 14:55:52 +0000

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,20 +2,20 @@
 from calculator_1 import add, div, mul, sub
 import sys
 
-result = 0
-
-arg_num = len(sys.argv)
-if arg_num != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-if arg_num == 4:
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    signs = sys.argv[2]
-elif (sys.argv[2] != '+' or sys.argv[2] != '-' or
-      sys.argv[2] != '/' or sys.argv[2] != '*'):
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+if __name__ == "__main__":
+    result = 0
+    arg_num = len(sys.argv)
+    if arg_num != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if arg_num == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        signs = sys.argv[2]
+    elif (sys.argv[2] != '+' or sys.argv[2] != '-' or
+          sys.argv[2] != '/' or sys.argv[2] != '*'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
 
 def sign(num_1, num_2, signs):
@@ -37,5 +37,4 @@ def sign(num_1, num_2, signs):
         exit(1)
 
 
-if __name__ == "__main__":
-    sign(a, b, signs)
+sign(a, b, signs)
