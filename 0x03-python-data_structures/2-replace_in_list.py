0890749e45cf16b12c706161cd61bde53ebc9e7a
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-15 02:33:58 +0100

    2. Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/ptyhon3
+
+def replace_in_list(my_list, idx, element):
+
+    length = len(my_list)
+    if (idx < 0) or (idx == 1 and length == 1):
+        return my_list
+    if idx >= length:
+        return my_list
+    else:
+        my_list.pop(idx)
+        my_list.insert(idx, element)
+        return my_list
