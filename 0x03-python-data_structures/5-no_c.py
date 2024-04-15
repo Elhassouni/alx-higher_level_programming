9699dd1d12296ef078f1d2896a5e85e17856c829
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-15 11:19:58 +0100

    5. Can you C me Now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string_list = []
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            new_string_list.append(i)
+            new_string = ''.join(new_string_list)
+    return new_string
