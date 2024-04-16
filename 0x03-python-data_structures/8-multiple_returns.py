b6b8fda3cf50d71dcf6b8a6cc11416cd3fe17363
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-16 01:41:08 +0100

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+        """Returns the length of the text and the first character."""
+        return len(sentence), sentence[0] if text else None
