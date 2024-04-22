69e17bc50bb1f60e7bfe09751bf2030e7731f392
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-22 22:29:23 +0100

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
+
     """Write a function that returns a key with the biggest integer value."""
+
     if a_dictionary is None:
         return None
     else:
-         max_value = max(a_dictionary.values())
-         for key, value in a_dictionary.items():
-             if value == max_value:
+        max_value = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value == max_value:
                 return key
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
