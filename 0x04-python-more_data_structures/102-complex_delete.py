3df483dad13e33ca89465240be06c7c9ff9b07d0
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-23 21:27:33 +0100

    15. Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = a_dictionary.copy()
-    for key, values in list(new_dict.items()):
+    """Write a function that prints a dictionary by ordered keys."""
+
+    new_key_dict = []
+    for key, values in a_dictionary.items():
         if values == value:
-            del new_dict[key]
+            new_key_dict.append(key)
+
+    for keys in new_key_dict:
+        del a_dictionary[keys]
+    return a_dictionary
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     """Write a function that prints a dictionary by ordered keys."""
-
+    if a_dictionary is None:
+        return None
     for key, value in sorted(a_dictionary.items()):
-        print(key + ":", value)
+        print(str(key) + ":", value)
