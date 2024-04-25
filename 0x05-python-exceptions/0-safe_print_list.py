a65517560e247d8d2b35a6e6cce56e8659da2a91
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-04-25 11:55:09 +0100

    i0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Write a function that prints x elements of a list."""
+    result = 0
+    try:
+        for v in my_list:
+            result += 1
+        if x >= result:
+            for key in my_list:
+                print(f'{key}', end='')
+            print()
+            return result
+        elif x < result and x != 0:
+            _sum = 0
+            for value in range(x):
+                print(f'{my_list[value]}', end='')
+                _sum += 1
+            print()
+            return _sum
+    except (ValueError, IndexError, TypeError):
+        print("x is zero Enter bigger number and list is empty")
