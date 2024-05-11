323884257730d8705dd8d87783f824bb2e44e674
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-11 01:01:31 +0100

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Square Class
+
+    This is an empty square class
+    Author: Ayoub EL-hassouni 
+
+    """
+
+""" This is a Module """
+
 class Square:
 """ Empty class Define square"""
     pass 
