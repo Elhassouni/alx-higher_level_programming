be61688a84f02bec3a386333158677c6ecd805c9
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-13 23:58:23 +0100

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square Class
+
+This is an empty square class
+Author:Ayoub
+
+"""
+
+""" This is a Module """
+
+
+class Square:
+    """ This is an Empty class """
+    def __init__(self, size):
+        self.size = size
