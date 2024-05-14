5aeddcb693986732c2313325cba3417442af8f55
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-14 01:08:16 +0100

    3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Square Class
+
+This is an empty square class
+Author: Ayoub
+
+"""
+
+""" This is a Module """
+
+
+class Square:
+    """ This is an Empty class """
+    def __init__(self, size=0):
+        """ check if the size is an integer """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        """check size """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """The type check and value check should be done before assigning the
+            value to the attribute."""
+        self.__size = size
+
+    def area(self):
+        """return the area of the square (size squared)"""
+        return self.__size ** 2
