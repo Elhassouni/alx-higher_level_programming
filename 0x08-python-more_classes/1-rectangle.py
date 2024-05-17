1989cebb5a2142f680305a4766ac601192e5cc53
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-17 12:41:52 +0100

    New fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Rectangle
+
+Class Rectangle that defines a rectangle by
+Author: Ayoub
+
+"""
+""" This is a Module """
+
 
 class Rectangle:
     """class Rectangle that defines a rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ getter and setter to be validated and set as private
+            Raise:
+                TypeError: if the width is not an integer.
+                ValueError: if the int is less then 0."""
         return self.__width
 
     @property
     def height(self):
+        """ setting private attribute using property
+            and chekching for type and value Error"""
         return self.__height
 
     @width.setter
