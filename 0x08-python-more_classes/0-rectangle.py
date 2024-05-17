7fc77b50a5cd7d167b6527b690c5f9e844c0d522
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-17 11:22:27 +0100

    0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Square Class
+
+This is an empty rectangle class
+Author: Ayoub EL-hassouni
+
+"""
+
+""" This is a Module """
+
+
+class Rectangle:
+    # empty class Rectangle that defines a rectangle:
+    pass
