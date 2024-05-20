4d57e999fafcf7a958a4ca973d064375cbf18d69
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-05-20 04:17:36 +0100

    edit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -96,3 +96,17 @@ class Rectangle:
         """ printing sting once instance delted"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+    
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2): 
+        if isinstance(rect_1, Rectangle):
+            if rect_1 > rect_2:
+                return rect_1
+            elif rect_2 > rect_2:
+                return rect_2
+            elif rect_1 == rect_2:
+                return rect_1
+        else:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
